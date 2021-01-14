4b3523fdc8e1b830f1dcc18e7786c41a44f2d23d
joy-shashwat <thisisjoyjacob@gmail.com>
2021-01-14 17:02:50 +0530

    removing residues

--- main.py
+++ main.py
@@ -92,4 +92,4 @@ def draw_rectangle(img, rect):
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='127.0.0.1', debug=True, port=5001)
